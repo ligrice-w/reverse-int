b82c0e81b19b20499cb98fedb55653e550fecfc3
Li Grice <ligrice.w@gmail.com>
2022-07-01 11:26:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    result = 0;
+    number = Math.abs(number);
+    while (number > 0) {
+        result = result * 10 + (number % 10);
+        number = Math.trunc(number / 10);
+    }
+    return result;
+};
